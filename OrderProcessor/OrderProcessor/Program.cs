e394aa2f9dc5caddde03ce7220225d6e4521d47a
Ankit Hotchandani <ankithotchandani@hotmail.com>
2022-01-12 12:29:26 +0530

    added class catogeries based on abstract design pattern

--- OrderProcessor/OrderProcessor/Program.cs
+++ OrderProcessor/OrderProcessor/Program.cs
@@ -2,6 +2,42 @@
 
 namespace OrderProcessor
 {
+    public abstract class Product
+    {
+
+    }
+
+    public abstract class PhysicalProduct : Product
+    {
+
+    }
+
+    public abstract class NonPhysicalProduct : Product
+    {
+
+    }
+
+    public class Book : PhysicalProduct
+    {
+
+    }
+
+    public class Membership : NonPhysicalProduct
+    {
+
+    }
+
+    public class Upgrade : NonPhysicalProduct
+    {
+
+    }
+
+    public class Video : NonPhysicalProduct
+    {
+
+    }
+
+
     class Program
     {
         static void Main(string[] args)
