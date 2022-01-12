d520dd66c0db5349af65217ad0cb806be23f0c3c
Ankit Hotchandani <ankithotchandani@hotmail.com>
2022-01-12 14:12:48 +0530

    added class properties and methods

--- OrderProcessor/OrderProcessor/Program.cs
+++ OrderProcessor/OrderProcessor/Program.cs
@@ -1,40 +1,97 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace OrderProcessor
 {
     public abstract class Product
     {
+        public List<string> Actions { get; set; }
+        public string Name { get; set; }
+        public abstract void GetSlip();
 
     }
 
     public abstract class PhysicalProduct : Product
     {
+        public PhysicalProduct()
+        {
+            Actions = new List<string>();
+            GetSlip();
+            GenerateCommision();
+        }
+        public override void GetSlip()
+        {
+            Actions.Add("Packaging Slip");
+        }
 
+        public virtual void GenerateCommision()
+        {
+            Actions.Add("Generated commision to the agent");
+        }
     }
 
     public abstract class NonPhysicalProduct : Product
     {
+        public NonPhysicalProduct()
+        {
+            Actions = new List<string>();
+        }
+        public override void GetSlip()
+        {
+            Actions.Add("Packaging Slip");
+        }
 
     }
 
     public class Book : PhysicalProduct
     {
-
+        public Book(string name)
+        {
+            this.Name = name;
+            GetSlip();
+            Actions.Add("Commision payment to agent");
+          
+        }
+        public override void GetSlip()
+        {            
+            Actions.Add("duplicate slip for royalty department");
+        }
     }
 
     public class Membership : NonPhysicalProduct
     {
-
+        public Membership(string name)
+        {
+            this.Name = name;
+            Actions.Add("Activate Membership");
+        }       
     }
 
     public class Upgrade : NonPhysicalProduct
     {
-
+        public Upgrade(string name)
+        {
+            this.Name = name;
+            Actions.Add("Upgrade Membership");
+        }
     }
 
     public class Video : NonPhysicalProduct
     {
+        public Video(string name)
+        {
+            this.Name = name;
+            GetSlip();
+        }
 
+        public override void GetSlip()
+        {
+            base.GetSlip();
+            if (this.Name == "Learning to Ski")
+            {
+                Actions.Add("Added Free First Aid Video");
+            }
+        }
     }
 
 
@@ -42,6 +99,7 @@ namespace OrderProcessor
     {
         static void Main(string[] args)
         {
+            var book = new Book("temp book");
             Console.WriteLine("Hello World!");
         }
     }
