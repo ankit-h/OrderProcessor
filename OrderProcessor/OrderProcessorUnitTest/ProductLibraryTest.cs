ba22d5964b3866ed0f43c6393d62f4eda17d5a99
Ankit Hotchandani <ankithotchandani@hotmail.com>
2022-01-12 18:30:25 +0530

    changed project to xunit test project

--- OrderProcessor/OrderProcessorUnitTest/ProductLibraryTest.cs
+++ OrderProcessor/OrderProcessorUnitTest/ProductLibraryTest.cs
@@ -1,11 +1,34 @@
-using Microsoft.VisualStudio.TestTools.UnitTesting;
+using FluentAssertions;
 using ProductLibrary;
+using System;
+using Xunit;
 
 namespace OrderProcessorUnitTest
 {
-    [TestClass]
     public class ProductLibraryTest
     {
-        
+        [Fact]
+        public void FactoryProducer_ForPhysicalProducer_ReturnsPhysicalProducerFactory()
+        {
+            //// Arange
+
+            //// Act
+            var procuderFactory = FactoryProducer.GetProductFactory(true);
+
+            //// Assert
+            procuderFactory.GetType().Should().Be(typeof(PhysicalProductFactory));
+        }
+
+        [Fact]
+        public void FactoryProducer_ForNonPhysicalProducer_ReturnsNonPhysicalProducerFactory()
+        {
+            //// Arange
+
+            //// Act
+            var procuderFactory = FactoryProducer.GetProductFactory(true);
+
+            //// Assert
+            procuderFactory.GetType().Should().Be(typeof(NonPhysicalProductFactory));
+        }
     }
 }
